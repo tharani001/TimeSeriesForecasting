477e5876669324fd44d34070c6829ce9199d2fe7
tharani001 <tharanisak63@gmail.com>
2025-03-26 21:42:51 +0530

    add multivariate forecasting models

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from statsmodels.stats.diagnostic import acorr_ljungbox
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.statespace.varmax import VARMAX
 
 from tqdm import tqdm_notebook
 from itertools import product
@@ -142,6 +143,104 @@ def SARIMAX_gridsearch(endog, exog, min_p, max_p, min_q, max_q, min_P, max_P, mi
     result_df.columns = ['(p,q,P,Q)', 'AIC']
     
     #Sort in ascending order, lower AIC is better
+    result_df = result_df.sort_values(by='AIC', ascending=True).reset_index(drop=True)
+    
+    return result_df
+
+
+
+def VAR_gridsearch(endog, min_p, max_p):
+    
+    all_p = range(min_p, max_p +1, 1)
+    
+    results = []
+    
+    print(f'Fitting {all_p} unique models')
+    
+    for p in tqdm_notebook(all_p):
+        try:
+            model = VARMAX(endog, order=(p, 0)).fit(dips=False)
+        except:
+            continue
+            
+        results.append([p, model.aic])
+        
+    result_df = pd.DataFrame(results)
+    result_df.columns = ['p', 'AIC']
+    
+    result_df = result_df.sort_values(by='AIC', ascending=True).reset_index(drop=True)
+    
+    return result_df
+
+def rolling_predictions(df, last_train_value, train_len, horizon, window, method):
+    
+    total_len = train_len + horizon
+    
+    if method == 'VAR':
+
+        cows_pred_VAR = []
+        calves_pred_VAR = []
+        
+        for i in range(train_len, total_len, window):
+            model = VARMAX(df[:i], order=(6,0))
+            res = model.fit(disp=False)
+            predictions = res.get_prediction(0, i + window - 1)
+            
+            oos_pred_cows = predictions.predicted_mean.iloc[-window:]['cows']
+            oos_pred_calves = predictions.predicted_mean.iloc[-window:]['calves']
+            
+            cows_pred_VAR.extend(oos_pred_cows)
+            calves_pred_VAR.extend(oos_pred_calves)
+            
+        cows_pred_VAR = np.insert(cows_pred_VAR, 0, last_train_value['cows'])
+        cows_pred_VAR = cows_pred_VAR.cumsum()
+        
+        calves_pred_VAR = np.insert(calves_pred_VAR, 0, last_train_value['calves'])
+        calves_pred_VAR = calves_pred_VAR.cumsum()
+        
+        return cows_pred_VAR[:horizon], calves_pred_VAR[:horizon]
+    
+    elif method == 'last':
+        cows_pred_last = []
+        calves_pred_last = []
+        
+        for i in range(train_len, total_len, window):
+            
+            cows_last = df[:i].iloc[-1]['cows']
+            calves_last = df[:i].iloc[-1]['calves']
+            
+            cows_pred_last.extend(cows_last for _ in range(window))
+            calves_pred_last.extend(calves_last for _ in range(window))
+        
+        cows_pred_last = np.insert(cows_pred_last, 0, last_train_value['cows'])
+        cows_pred_last = cows_pred_last.cumsum()
+        
+        calves_pred_last = np.insert(calves_pred_last, 0, last_train_value['calves'])
+        calves_pred_last = calves_pred_last.cumsum()
+            
+        return cows_pred_last[:horizon], calves_pred_last[:horizon]
+    
+def VARMA_gridsearch(endog, min_p, max_p, min_q, max_q):
+    
+    all_p = range(min_p, max_p+1, 1)
+    all_q = range(min_q, max_q+1, 1)
+    all_orders = list(product(all_p, all_q))
+    
+    results = []
+    
+    print(f'Fitting {len(all_orders)} unique models')
+    
+    for order in tqdm_notebook(all_orders):
+        try:
+            model = VARMAX(endog, order=order).fit(disp=False)
+        except:
+            continue
+    
+        results.append([order, model.aic])
+        
+    result_df = pd.DataFrame(results)
+    result_df.columns = ['(p,q)', 'AIC']
+    
     result_df = result_df.sort_values(by='AIC', ascending=True).reset_index(drop=True)
     
     return result_df
